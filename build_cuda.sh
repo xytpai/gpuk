e1d08389bd8d1cf4e45c9efb9dee40245ad28999
xytpai <xytpai@foxmail.com>
2025-12-17 08:40:57 +0000

    change to sm90

--- build_cuda.sh
+++ build_cuda.sh
@@ -1,1 +1,1 @@
-nvcc -x cu -O3 $2 --std=c++20 --expt-relaxed-constexpr -arch sm_80 -Isrc/common -Isrc/impl $1 -o a.out
+nvcc -x cu -O3 $2 --std=c++20 --expt-relaxed-constexpr -arch sm_90 -Isrc/common -Isrc/impl $1 -o a.out
