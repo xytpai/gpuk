93109747c1dedd241f87920a793397dcc0a81a73
xytpai <xytpai@foxmail.com>
2025-12-25 09:47:42 +0000

    update ar test

--- test/cpp/test_all_reduce.cpp
+++ test/cpp/test_all_reduce.cpp
@@ -1,8 +1,261 @@
-#include "all_reduce_fusion_impl.h"
+#include <array>
+#include <cassert>
+#include <chrono>
+#include <condition_variable>
+#include <functional>
+#include <iostream>
+#include <mutex>
+#include <random>
+#include <stdlib.h>
+#include <thread>
+#include <time.h>
+#include <tuple>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+#if defined(__HIPCC__)
+#include <hip/hip_bf16.h>
+#include <hip/hip_cooperative_groups.h>
+#include <hip/hip_fp16.h>
+#include <hip/hip_runtime.h>
+#include <hip/hip_runtime_api.h>
+#define gpuSuccess hipSuccess
+#define gpuMemcpy hipMemcpy
+#define gpuMemcpyAsync hipMemcpyAsync
+#define gpuMemset hipMemset
+#define gpuMemcpyDeviceToHost hipMemcpyDeviceToHost
+#define gpuMemcpyHostToDevice hipMemcpyHostToDevice
+#define gpuMemcpyDeviceToDevice hipMemcpyDeviceToDevice
+#define gpuMalloc hipMalloc
+#define gpuFree hipFree
+#define gpuDeviceSynchronize hipDeviceSynchronize
+#define gpuSetDevice hipSetDevice
+#define gpuGetDevice hipGetDevice
+#define gpuGetDeviceCount hipGetDeviceCount
+#define gpuMemcpyPeerAsync hipMemcpyPeerAsync
+#define gpuDeviceCanAccessPeer hipDeviceCanAccessPeer
+#define gpuDeviceEnablePeerAccess hipDeviceEnablePeerAccess
+#define gpuEvent_t hipEvent_t
+#define gpuEventCreate hipEventCreate
+#define gpuEventDestroy hipEventDestroy
+#define gpuEventRecord hipEventRecord
+#define gpuEventSynchronize hipEventSynchronize
+#define gpuEventElapsedTime hipEventElapsedTime
+#define gpuStream_t hipStream_t
+#define gpuStreamCreate hipStreamCreate
+#define gpuStreamDestroy hipStreamDestroy
+#define gpuStreamSynchronize hipStreamSynchronize
+#define gpuFuncAttributes hipFuncAttributes
+#define gpuFuncGetAttributes hipFuncGetAttributes
+#define gpuDeviceGetAttribute hipDeviceGetAttribute
+#define gpuDevAttrMaxRegistersPerBlock hipDeviceAttributeMaxRegistersPerBlock
+#define gpuDevAttrMultiProcessorCount hipDeviceAttributeMultiprocessorCount
+#define gpuLaunchCooperativeKernel hipLaunchCooperativeKernel
+#define __bfloat16 __hip_bfloat16
+#define __bfloat16_raw __hip_bfloat16_raw
+#define gpuIpcMemHandle_t hipIpcMemHandle_t
+#define gpuIpcGetMemHandle hipIpcGetMemHandle
+#define gpuIpcOpenMemHandle hipIpcOpenMemHandle
+#define gpuIpcMemLazyEnablePeerAccess hipIpcMemLazyEnablePeerAccess
+#define gpuPointerGetAttribute hipPointerGetAttribute
+#define GPU_POINTER_ATTRIBUTE_RANGE_START_ADDR HIP_POINTER_ATTRIBUTE_RANGE_START_ADDR
+#define gpuDeviceptr_t hipDeviceptr_t
+#define gpuStreamCaptureStatus hipStreamCaptureStatus
+#define gpuStreamIsCapturing hipStreamIsCapturing
+#define gpuStreamCaptureStatusActive hipStreamCaptureStatusActive
+#endif
+#if defined(__CUDACC__)
+#include <cooperative_groups.h>
+#include <cuda_bf16.h>
+#include <cuda_fp16.h>
+#include <cuda_runtime.h>
+#include <cuda_runtime_api.h>
+#define gpuSuccess cudaSuccess
+#define gpuMemcpy cudaMemcpy
+#define gpuMemcpyAsync cudaMemcpyAsync
+#define gpuMemset cudaMemset
+#define gpuMemcpyDeviceToHost cudaMemcpyDeviceToHost
+#define gpuMemcpyHostToDevice cudaMemcpyHostToDevice
+#define gpuMemcpyDeviceToDevice cudaMemcpyDeviceToDevice
+#define gpuMalloc cudaMalloc
+#define gpuFree cudaFree
+#define gpuDeviceSynchronize cudaDeviceSynchronize
+#define gpuSetDevice cudaSetDevice
+#define gpuGetDevice cudaGetDevice
+#define gpuGetDeviceCount cudaGetDeviceCount
+#define gpuMemcpyPeerAsync cudaMemcpyPeerAsync
+#define gpuDeviceCanAccessPeer cudaDeviceCanAccessPeer
+#define gpuDeviceEnablePeerAccess cudaDeviceEnablePeerAccess
+#define gpuEvent_t cudaEvent_t
+#define gpuEventCreate cudaEventCreate
+#define gpuEventDestroy cudaEventDestroy
+#define gpuEventRecord cudaEventRecord
+#define gpuEventSynchronize cudaEventSynchronize
+#define gpuEventElapsedTime cudaEventElapsedTime
+#define gpuStream_t cudaStream_t
+#define gpuStreamCreate cudaStreamCreate
+#define gpuStreamDestroy cudaStreamDestroy
+#define gpuStreamSynchronize cudaStreamSynchronize
+#define gpuFuncAttributes cudaFuncAttributes
+#define gpuFuncGetAttributes cudaFuncGetAttributes
+#define gpuDeviceGetAttribute cudaDeviceGetAttribute
+#define gpuDevAttrMaxRegistersPerBlock cudaDevAttrMaxRegistersPerBlock
+#define gpuDevAttrMultiProcessorCount cudaDevAttrMultiProcessorCount
+#define gpuLaunchCooperativeKernel cudaLaunchCooperativeKernel
+#define __bfloat16 __nv_bfloat16
+#define __bfloat16_raw __nv_bfloat16_raw
+#define gpuIpcMemHandle_t cudaIpcMemHandle_t
+#define gpuIpcGetMemHandle cudaIpcGetMemHandle
+#define gpuIpcOpenMemHandle cudaIpcOpenMemHandle
+#define gpuIpcMemLazyEnablePeerAccess cudaIpcMemLazyEnablePeerAccess
+#define gpuPointerGetAttribute cudaPointerGetAttribute
+#define GPU_POINTER_ATTRIBUTE_RANGE_START_ADDR CU_POINTER_ATTRIBUTE_RANGE_START_ADDR
+#define gpuDeviceptr_t cudaDeviceptr_t
+#define gpuStreamCaptureStatus cudaStreamCaptureStatus
+#define gpuStreamIsCapturing cudaStreamIsCapturing
+#define gpuStreamCaptureStatusActive cudaStreamCaptureStatusActive
+#endif
 
 namespace test {
 
 namespace comm_utils {
+
+namespace details {
+
+static constexpr int kBytesPerAccess = 16;
+
+template <bool RELAXED = true>
+__device__ __forceinline__ void st_flag(int *addr, int flag) {
+#ifdef __CUDACC__
+    asm volatile("st.global.release.sys.b32 [%1], %0;" ::"r"(flag), "l"(addr));
+#else
+    __scoped_atomic_store_n(addr, flag,
+                            RELAXED ? __ATOMIC_RELAXED : __ATOMIC_RELEASE,
+                            __MEMORY_SCOPE_SYSTEM);
+#endif
+}
+
+template <bool RELAXED = true>
+__device__ __forceinline__ int ld_flag(int *addr) {
+    int flag;
+#ifdef __CUDACC__
+    asm volatile("ld.global.acquire.sys.b32 %0, [%1];"
+                 : "=r"(flag)
+                 : "l"(addr));
+#else
+    flag = __scoped_atomic_load_n(addr,
+                                  RELAXED ? __ATOMIC_RELAXED : __ATOMIC_ACQUIRE,
+                                  __MEMORY_SCOPE_SYSTEM);
+#endif
+    return flag;
+}
+
+} // namespace details
+
+template <typename T, int VEC_SIZE>
+struct alignas(sizeof(T) * VEC_SIZE) vec_t {
+    T data[VEC_SIZE];
+    __device__ __forceinline__ T &operator[](int i) {
+        return data[i];
+    }
+    __device__ __forceinline__ T const &operator[](int i) const {
+        return data[i];
+    }
+    __device__ __forceinline__ void load(const T *ptr) {
+        *this = *reinterpret_cast<vec_t<T, VEC_SIZE> *>(const_cast<T *>(ptr));
+    }
+    __device__ __forceinline__ void store(T *ptr) {
+        *reinterpret_cast<vec_t<T, VEC_SIZE> *>(ptr) = *this;
+    }
+    __device__ __forceinline__ void fill(T val) {
+#pragma unroll
+        for (int i = 0; i < VEC_SIZE; ++i) {
+            data[i] = val;
+        }
+    }
+    template <typename VT>
+    __device__ __forceinline__ void cast_fill(VT val) {
+#pragma unroll
+        for (int i = 0; i < VEC_SIZE; ++i) {
+            *reinterpret_cast<VT *>(&data[i]) = val;
+        }
+    }
+};
+
+inline int enable_p2p() {
+    int ngpus = 0;
+    gpuGetDeviceCount(&ngpus);
+    for (int local = 0; local < ngpus; ++local) {
+        gpuSetDevice(local);
+        for (int peer = 0; peer < ngpus; ++peer) {
+            if (local == peer)
+                continue;
+            int can = 0;
+            gpuDeviceCanAccessPeer(&can, local, peer);
+            assert(can);
+            gpuDeviceEnablePeerAccess(peer, 0);
+        }
+    }
+    return ngpus;
+}
+
+static constexpr int MAX_RANKS = 8;
+static constexpr int NBLOCKS_PER_GPU = 80;
+
+template <int NRanks>
+struct CommDeviceMeta {
+    void *barrier_flag_ptrs[NRanks];
+    void *sync_clock;
+    int rank;
+    int nranks;
+};
+
+struct CommMeta {
+    void *barrier_flag_ptrs[MAX_RANKS];
+    void *sync_clock;
+    int rank;
+    int nranks;
+};
+
+struct CommPtrs {
+    void *data_ptrs[MAX_RANKS];
+};
+
+template <int NRanks>
+struct SyncComm {
+    __device__ __forceinline__ SyncComm(CommDeviceMeta<NRanks> &meta) {
+        flag_ptr = ((int *)meta.sync_clock) + blockIdx.x;
+        int rank = meta.rank;
+        if (threadIdx.x < NRanks) {
+            int target_rank = threadIdx.x;
+            target_flag = reinterpret_cast<int *>(meta.barrier_flag_ptrs[target_rank]) + blockIdx.x * NRanks + rank;
+            current_flag = reinterpret_cast<int *>(meta.barrier_flag_ptrs[rank]) + blockIdx.x * NRanks + target_rank;
+        }
+        flag = *flag_ptr;
+    }
+
+    template <bool RELAXED = true, bool FINAL = true>
+    __device__ __forceinline__ void sync() {
+        __syncthreads();
+        flag += 1;
+        if (threadIdx.x < NRanks) {
+            details::st_flag<RELAXED>(target_flag, flag);
+            while (details::ld_flag<RELAXED>(current_flag) < flag) {
+            }
+        }
+        __syncthreads();
+        if constexpr (FINAL) {
+            if (threadIdx.x == 0) {
+                *flag_ptr = flag;
+            }
+        }
+    }
+
+    int *flag_ptr;
+    int *target_flag;
+    int *current_flag;
+    int flag;
+};
 
 class Communicator {
 public:
@@ -99,8 +352,8 @@ template <typename T, int NRanks, int BLOCK_SIZE>
 __global__ void __launch_bounds__(BLOCK_SIZE, 1) allreduce_direct_kernel(int size, CommDeviceMeta<NRanks> meta, CommPtrs cptrs) {
     static constexpr int VEC_SIZE = details::kBytesPerAccess / sizeof(T);
     SyncComm<NRanks> comm(meta);
-    using vec_t_ = kernel_utils::vec_t<T, VEC_SIZE>;
-    using acc_vec_t_ = kernel_utils::vec_t<float, VEC_SIZE>;
+    using vec_t_ = vec_t<T, VEC_SIZE>;
+    using acc_vec_t_ = vec_t<float, VEC_SIZE>;
     comm.template sync<true, false>();
     vec_t_ vec;
     acc_vec_t_ acc_vec;
@@ -346,7 +599,7 @@ std::tuple<bool, float, float> runbench(int nranks, int size, bool validate, flo
 } // namespace test
 
 int main() {
-    int nranks = comm_utils::enable_p2p();
+    int nranks = test::comm_utils::enable_p2p();
     std::cout << "nranks:" << nranks << "\n";
     std::vector<int> warmup_sizes = {1024 * 1024, 1024 * 1024};
     for (auto size : warmup_sizes) {
